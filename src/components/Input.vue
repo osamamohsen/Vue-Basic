bc8366fa97f3b5921085aedf950b3b01fe801cfe
osamamohsen <osamamohsen1994@gmail.com>
2017-05-27 18:34:33 +0200

    add vue basic

---
+++ src/components/Input.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Welcome in Vue JS wsd</h2>
+        <input type="text" :value="msg" @input="changeMessage"/>
+        <h4>{{ message }}</h4>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['msg'],
+        data () {
+            return {
+                message: ''
+            }
+        },
+        methods: {
+            changeMessage(event){
+                this.message = event.target.value;
+                this.$emit('messageChanged',this.message);
+            }
+        }
+    }
+</script>
---
+++ src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <app-input :msg="message" @messageChanged="message = $event"></app-input>
+    </div>
+</template>
+
+<script>
+    import Input from './Input';
+    export default {
+        data() {
+            return {
+                message: 'hallo world'
+            }
+        },
+        components: {
+            'app-input' : Input
+        }
+    }
+</script>
